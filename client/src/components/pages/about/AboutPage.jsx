8e5164dbe9d98869f2301f8759da2488b266fae6
Lucy Guerra <lucycarranza430@gmail.com>
2025-10-10 13:02:25 -0600

    Ajustes AboutPage

--- client/src/components/pages/about/AboutPage.jsx
+++ client/src/components/pages/about/AboutPage.jsx
@@ -4,21 +4,19 @@ import { aboutData } from '../../../data/aboutData';
 
 const AboutPage = () => {
   return (
-    <div 
-      className="h-screen relative flex flex-col"
-      style={{
-        backgroundImage: 'url(/fondo_info.png)',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat'
-      }}
-    >
+    <div className="min-h-screen relative flex flex-col">
+      {/* Background Image */}
+      <img 
+        src="/fondo_info.png" 
+        alt="Background" 
+        className="absolute inset-0 w-full h-full object-cover z-0"
+      />
       
       {/* Header */}
       <Header />
       
       {/* Main Content */}
-      <main className="relative z-10 w-full px-8 flex-1 flex items-center justify-center">
+      <main className="relative z-10 w-full px-6 md:px-8 flex-1 flex items-center justify-center py-4">
         <ContentBox data={aboutData} />
       </main>
     </div>
