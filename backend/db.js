fd462036e88e013779c461fdd458d11a7f0dfea3
celestemendez007 <keylen.mendez@keyinstitute.edu.sv>
2025-10-10 12:16:27 -0600

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -10,4 +10,12 @@ export const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  max: 10, // máximo de conexiones
+  idleTimeoutMillis: 30000, // 30 seg para cerrar conexiones inactivas
+  connectionTimeoutMillis: 2000 // espera hasta 2 seg para conectarse
+});
+
+pool.on('error', (err) => {
+  console.error('Error inesperado en cliente idle', err);
+  process.exit(-1);
 });
